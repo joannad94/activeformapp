422e13b596853a5271864a540719c94715f6aca1
Joanna Daszkiewicz <asia.daszkiewicz@wp.pl>
2020-08-13 11:33:34 +0200

    Updates

---
+++ precache-manifest.4d5d3cff717d1b0b67969cecd8619480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b182eebf97194960e5235d6058bf76",
+    "url": "/activeformapp/index.html"
+  },
+  {
+    "revision": "1bcae96f0e9ff798e447",
+    "url": "/activeformapp/static/css/main.2a50a523.chunk.css"
+  },
+  {
+    "revision": "aef0cd337ab75bf8cbaa",
+    "url": "/activeformapp/static/js/2.aeaf722f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/activeformapp/static/js/2.aeaf722f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bcae96f0e9ff798e447",
+    "url": "/activeformapp/static/js/main.a2a5d2d4.chunk.js"
+  },
+  {
+    "revision": "88d588154aa02e4c4e7d",
+    "url": "/activeformapp/static/js/runtime-main.c98a1974.js"
+  },
+  {
+    "revision": "a87d0f352b87d3736e3487e389f37ff7",
+    "url": "/activeformapp/static/media/logo.a87d0f35.svg"
+  }
+]);
